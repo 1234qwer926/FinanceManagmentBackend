508c1c28d99e4e220927bf7446eb0ff584e3a4ad
1234qwer926 <srao50872@gmail.com>
2025-03-08 21:55:32 +0530

    final changes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db.js")
+const cors = require("cors");
+const connectDB = require("./config/db.js");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",  
+  credentials: true  
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
